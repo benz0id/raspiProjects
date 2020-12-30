1ea6af90ec86cc8dd1817d5cac0b5fee4cbb7917
bfern <benjamin.tudorprice@mail.utoronto.ca>
2020-12-30 15:52:51 -0500

    minor debug to file system

--- RFIDTesting/Debug.py
+++ RFIDTesting/Debug.py
@@ -4,7 +4,7 @@ from User import User
 
 log_manager = LogManager()
 
-log_manager.register_new_user(User["Jared", 13, 0])
+log_manager.register_new_user(User(["Jared", 13, 0]))
 user = log_manager.get_user(0)
 
 print(user.get_username())
