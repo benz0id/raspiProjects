82ed2d84194edf8b104a501a692b33024e92519a
bfern <benjamin.tudorprice@mail.utoronto.ca>
2020-12-30 20:33:29 -0500

    restructuring and finalisation of LCD implementation

--- RFIDTesting/LogManager.py
+++ RFIDTesting/LogManager.py
@@ -93,7 +93,7 @@ class LogManager:
 
         login_str += "Welcome back " + user.get_username() + "\n" + \
                      "Your last login was at:\n" + \
-                     str(last_login.strftime("%b %d %Y at %I:%M%p")) + \
+                     str(last_login.strftime("%b %d %Y at %I:%M%p")) + "\n" + \
                      time_since_last_login_str
 
         return login_str
