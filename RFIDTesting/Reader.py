3c3c04e777aa668d73fc06cbf3c23d2e03897481
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-01-05 15:31:00 -0500

    removed encryption

--- RFIDTesting/Reader.py
+++ RFIDTesting/Reader.py
@@ -6,9 +6,10 @@ from cryptography.fernet import Fernet, InvalidToken
 import os.path
 import logging
 
-reader = SimpleMFRC522()
 GPIO.setwarnings(False)
-
+# Encryption removed as encrypted string is too long to be stored in standard
+# RFID tag, may be re-implemented if more efficient algorithm is found
+"""
 # Load key from file if it already exists, otherwise create a file and key
 key_file_path = os.path.join("Users", "Key.txt")
 
@@ -23,21 +24,24 @@ except FileNotFoundError:
     key_file.close()
 
 f = Fernet(key)
+"""
 
 
 class Reader:
+    """Adapts the SimpleMFRC522 reader"""
     reader = SimpleMFRC522()
 
     def write(self, string: str):
         """Writes a given string to the tag"""
         try:
-            reader.write(string)
+            self.reader.write(string)
         finally:
             GPIO.cleanup()
 
     def get_tag_data(self) -> str:
         """Interprets and returns the data stored in the tag.
         #TODO add encryption to this function"""
+        """
         read_data = self.read_data()
         try:
             decr_data = f.decrypt(read_data.encode())
@@ -47,12 +51,13 @@ class Reader:
         except InvalidToken:
             print("Improperly encrypted data received: " + read_data)
             logging.info("Improperly encrypted data received")
-            return ""
+            return """""
+        return self.read_data()
 
     def read_data(self) -> str:
         """Reads the data input from the RFID"""
         try:
-            tag_id, user_info = reader.read()
+            tag_id, user_info = self.reader.read()
         finally:
             GPIO.cleanup()
         # TODO add a logger for all tag_ids added
@@ -62,15 +67,27 @@ class Reader:
         """Writes new user info to an RFID tag. Waits for a tag to contact.
         Written info format:
         'entered_username|last_login_datetime('%Y-%m-%d %H:%M:%S')' """
-        user_info = self.encrypt_user_data(user)
+        # user_info = self.encrypt_user_data(user)
+        user_info = self.get_user_data(user)
 
         try:
-            reader.write(str(user_info))
+            self.reader.write(str(user_info))
         finally:
             GPIO.cleanup()
 
-    def encrypt_user_data(self, user: User) -> bytes:
-        """Encrypts a user's information into a token"""
+    """def encrypt_user_data(self, user: User) -> bytes:
+        Encrypts a user's information into a token
+        user_info = self.get_user_data(user)
+        
+        user_info = user_info[0:-1]
+        user_bytes = user_info.encode()
+        encr_user_data = f.encrypt(user_bytes)
+        print("User data encrypted to" + str(encr_user_data))
+
+        return encr_user_data"""
+
+    def get_user_data(self, user: User):
+        """Gets a string representation of the user's info"""
         user_info = ""
         attributes = [
             user.get_username(),
@@ -82,9 +99,4 @@ class Reader:
             user_info += str(attribute)
             user_info += "|"
 
-        user_info = user_info[0:-1]
-        user_bytes = user_info.encode()
-        encr_user_data = f.encrypt(user_bytes)
-        print("User data encrypted to" + str(encr_user_data))
-
-        return encr_user_data
+        return user_info[0:-1]
