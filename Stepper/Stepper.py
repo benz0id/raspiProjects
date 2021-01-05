0448e7d037a6a7bc631a7eaf5a7456b9a7a120ac
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-01-05 17:32:34 -0500

    adding stepperawd

--- Stepper/Stepper.py
+++ Stepper/Stepper.py
@@ -35,10 +35,7 @@ StepDir = 1  # Set to 1 or 2 for clockwise
 # Set to -1 or -2 for anti-clockwise
 
 # Read wait time from command line
-if len(sys.argv) > 1:
-    WaitTime = int(sys.argv[1]) / float(1000)
-else:
-    WaitTime = 10 / float(1000)
+WaitTime = float(0.01)
 
 # Initialise variables
 StepCounter = 0
