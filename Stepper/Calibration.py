5fb2781fcf16f2639fc8115558f7b7ee479be32c
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-01-06 17:42:02 -0500

    awdawdasda

--- Stepper/Calibration.py
+++ Stepper/Calibration.py
@@ -2,6 +2,6 @@ from CalibrationStepper import CalibrationStepper, SEQ_28BYJ_28
 from math import pi
 
 step_pins = [19, 26, 16, 20]
-stepper = CalibrationStepper(step_pins, 5000, SEQ_28BYJ_28)
+stepper = CalibrationStepper(step_pins, 1500, SEQ_28BYJ_28)
 print("Turning the stepper")
 stepper.turn(1, 0.25, 2 * pi)
