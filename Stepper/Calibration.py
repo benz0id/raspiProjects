baffbc7b8cc44862296a95facdfb3bd2028937c8
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-01-06 20:44:38 -0500

    increased efficiencies

--- Stepper/Calibration.py
+++ Stepper/Calibration.py
@@ -10,6 +10,6 @@ step_pins = [19, 26, 16, 20]
 stepper = CalibrationStepper(step_pins, 2048, seq, mode)
 stepper1 = Stepper(step_pins, 2048, seq, mode)
 print("Turning the cstepper")
-stepper.turn(1, pi / 2, 2 * pi)
+stepper.turn(1, pi / 2, 2 * pi, simple_half_step=True)
 print("Turning the stepper")
-stepper1.turn(1, pi / 2, 30 * 2 * pi)
+stepper1.turn(1, pi / 2, 30 * 2 * pi, simple_half_step=True)
