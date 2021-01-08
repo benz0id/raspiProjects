e73a37a37b07383b33d92e6f48a693c2736882e9
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-01-08 01:53:22 -0500

    Donezo

--- add_new_user.py
+++ add_new_user.py
@@ -3,9 +3,12 @@ from devices.presenters import ConsolePresenter
 from devices.reader import Reader
 from login.security_manager import SecurityManager
 from login.log_record_manager import LogManager
+import RPi.GPIO as GPIO
 
 um = UserManager(ConsolePresenter(), Reader(), SecurityManager(), LogManager())
 
 um.register_new_user()
+
+GPIO.cleanup()
 
 
