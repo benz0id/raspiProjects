54cd0e36a0b89e1bbf8fbb5df3c60cfe0a74ecba
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-01-08 01:42:32 -0500

    Donezo

--- run.py
+++ run.py
@@ -1,23 +1,28 @@
 import logging
 
-from devices import device_controller, lock, misc_info, presenters, reader, \
-    stepper
-from login.rfid_login_controller import RFIDLoginController
 import RPi.GPIO as GPIO
+
+from device_controller import DeviceController
+from lock import Lock
+from login.rfid_login_controller import RFIDLoginController
+from misc_info import LOCK_DIRECTION, LOCK_TURNS, STEPPER_PINS
+from presenters import LCD
+from reader import Reader
+from devices.stepper import NUM_STEPS_28BYJ_28, SEQ_HALF_28BYJ_28, Stepper
 
 GPIO.setmode(GPIO.BCM)
 logging.basicConfig(filename="logs.log", level=logging.DEBUG)
 
-stepper_motor = stepper.Stepper(misc_info.STEPPER_PINS,
-                                stepper.NUM_STEPS_28BYJ_28,
-                                stepper.SEQ_HALF_28BYJ_28)
-presenter = presenters.LCD()
-rfid_reader = reader.Reader()
+stepper_motor = Stepper(STEPPER_PINS,
+                                NUM_STEPS_28BYJ_28,
+                                SEQ_HALF_28BYJ_28)
+presenter = LCD()
+rfid_reader = Reader()
 
-lock = lock.Lock(stepper_motor, misc_info.LOCK_DIRECTION, misc_info.LOCK_TURNS)
+lock = Lock(stepper_motor, LOCK_DIRECTION, LOCK_TURNS)
 
 rfid_controller = RFIDLoginController(rfid_reader, presenter)
-devices = device_controller.DeviceController(lock, rfid_reader, presenter)
+devices = DeviceController(lock, rfid_reader, presenter)
 
 rfid_controller.attach(devices)
 
