5a983a64944678428b413e67dd1e2ff3e363453e
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-01-08 01:43:37 -0500

    Donezo

--- run.py
+++ run.py
@@ -2,12 +2,12 @@ import logging
 
 import RPi.GPIO as GPIO
 
-from device_controller import DeviceController
-from lock import Lock
+from devices.device_controller import DeviceController
+from devices.lock import Lock
 from login.rfid_login_controller import RFIDLoginController
-from misc_info import LOCK_DIRECTION, LOCK_TURNS, STEPPER_PINS
-from presenters import LCD
-from reader import Reader
+from devices.misc_info import LOCK_DIRECTION, LOCK_TURNS, STEPPER_PINS
+from devices.presenters import LCD
+from devices.reader import Reader
 from devices.stepper import NUM_STEPS_28BYJ_28, SEQ_HALF_28BYJ_28, Stepper
 
 GPIO.setmode(GPIO.BCM)
