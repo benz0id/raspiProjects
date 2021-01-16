b08ea2fd994892a2544c575c785c93a36cdc0be7
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-01-16 18:48:51 -0500

    forgot to import logging
    (cherry picked from commit 1e6e383548770d0ab8c2ad07354cc9b6bbc38dae)

--- door_lock_system/run.py
+++ door_lock_system/run.py
@@ -2,14 +2,14 @@ import logging
 
 import RPi.GPIO as GPIO
 
-from device_controller import DeviceController
+from devices.device_controller import DeviceController
 from lock import Lock
 from rfid_login_controller import RFIDLoginController
 from misc_info import GREEN_LED_PIN, LOCK_DIRECTION, LOCK_TURNS, \
     RED_LED_PIN, STEPPER_PINS, STEPPER_SPEED, LOCK_BUTTON
 from door_lock_system.devices import LCD
 from door_lock_system.devices import Reader
-from stepper import NUM_STEPS_28BYJ_28, SEQ_HALF_28BYJ_28, Stepper
+from devices.stepper import NUM_STEPS_28BYJ_28, SEQ_HALF_28BYJ_28, Stepper
 from led_manager import LedManager
 from threading import Thread
 
