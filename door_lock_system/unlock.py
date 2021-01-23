b28cd824382746928110bc6344feb90d92942ee4
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-01-22 20:11:23 -0500

    dwad

---
+++ door_lock_system/unlock.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+from devices.lock import Lock
+from devices.misc_info import LOCK_DIRECTION, LOCK_TURNS, STEPPER_PINS, \
+    STEPPER_SPEED
+from devices.stepper import NUM_STEPS_28BYJ_28, SEQ_HALF_28BYJ_28, Stepper
+
+GPIO.setmode(GPIO.BCM)
+
+stepper_motor = Stepper(STEPPER_PINS,
+                        NUM_STEPS_28BYJ_28,
+                        SEQ_HALF_28BYJ_28)
+
+lock = Lock(stepper_motor, LOCK_DIRECTION, LOCK_TURNS, STEPPER_SPEED)
+
+lock.unlock()
